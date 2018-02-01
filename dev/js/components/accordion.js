e88bd81822ed7f7c946b734fa2d9cf5e60fd1e60
IPGAP\frank.wang <frank.wang@mullenloweprofero.com>
2018-02-01 16:44:53 +0800

    accordion module

--- dev/js/components/accordion.js
+++ dev/js/components/accordion.js
@@ -1,25 +1,58 @@
 export default class {
-    constructor(element, APP) {
+    constructor(element) {
+        this.flag = false;
         this.element = element;
-        this.items = this.element.querySelectorAll(".icon");
+        this.dropDowns = element.querySelectorAll(".accordion_item");
     }
 
-    clickItem(target) {
-        var _this = this;
-        for (var i = 0; i < this.items.length; i++) {
-            _this.items[i].addEventListener("click", () => {
-                _this.innerText = " ";
-                _this.innerText = "+";
-                console.log(this);
-                console.log(_this);
-                console.log(this.parentElement);
-                console.log(_this.parentElement);
-           }, true)
-        }
+    collapseAll() {
+        let _this = this;
+        let items = this.element.querySelectorAll('.js-accordion-item:not(.is-active)');
+        items.forEach((item) => {
+           let dropDown = item.querySelector('.js-accordion-content');
+           item.classList.remove('is-open');
+           dropDown.setAttribute('aria-hidden', true);
+        });
     };
 
-    init() {
-        this.clickItem(this);
+    changeIcon() {
+        let items = this.dropDowns;
+        for (let i = 0, j = items.length; i < j; i++) {
+            if (items[i].classList.contains('is-open')) {
+                items[i].querySelectorAll('.js-accordion-trigger')[0].innerHTML = '';
+                items[i].querySelectorAll('.js-accordion-trigger')[0].innerHTML = '-';
+            } else {
+                items[i].querySelectorAll('.js-accordion-trigger')[0].innerHTML = '';
+                items[i].querySelectorAll('.js-accordion-trigger')[0].innerHTML = '+';
+            }
+        }
+
     }
 
+    init() {
+        this.element.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            if (event.target.classList.contains('js-accordion-trigger')){
+                let item = event.target.closest(".js-accordion-item");
+                let dropDown = item.querySelectorAll(".js-accordion-content");
+
+                item.classList.add('is-active');
+                item.classList.toggle('is-open');
+
+                this.collapseAll();
+
+                if (item.classList.contains('is-open')) {
+                    this.flag = true;
+                    dropDown[0].setAttribute('aria-hidden', false);
+                } else {
+                    this.flag = false;
+                    dropDown[0].setAttribute('aria-hidden', true);
+                }
+
+                this.changeIcon();
+                item.classList.remove('is-active');
+            }
+        });
+    }
 }
