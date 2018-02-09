4a1a8dfccf046bf85250dc20c2eb55b5ce70837a
IPGAP\frank.wang <frank.wang@mullenloweprofero.com>
2018-02-09 15:15:48 +0800

    demo for project

--- dev/js/components/carousel.js
+++ dev/js/components/carousel.js
@@ -3,21 +3,40 @@ export default class {
         this.element = element;
         this.v_input = this.element.querySelectorAll('.v_input') || [];
         this.r_input = this.element.querySelectorAll('.reall_input') || [];
+        
+        this.p_post = this.element.querySelectorAll('#trigger_ajax');
+        this.p_pros = this.element.querySelectorAll('#pros')[0];
     }
 
     init () {
-        console.log('aaa');
-
-        this.v_input[0].addEventListener('change', (event) => {
+        this.element.addEventListener('click', (event) => {
             console.log(event);
+            let _target = event.target.previousElementSibling;
+            _target.focus();
+            _target.addEventListener('input', () => {
+                console.log('change');
+            });
+            _target.addEventListener('change', () => {
+                console.log('change');
+            });
         }, false);
 
-        this.v_input[0].onchange = () => {
-            console.log('change')
-        }
+        this._click();
     }
 
-    _focus() {
+    _click() {
+        this.element.addEventListener('click', (event) => {
+            let xhr = new XMLHttpRequest();
 
+            xhr.onprogress = (event) => {
+                if (event.lengthComputable) {
+                    let loaded = parseInt(event.loaded/event.total*100) + "%";
+                    this.p_pros.style.width = loaded;
+                    this.p_pros.innerHTML   = loaded;
+                }
+            }
+            xhr.open("post", "for_text.php", true);
+            xhr.send(null);
+        })
     }
 }
